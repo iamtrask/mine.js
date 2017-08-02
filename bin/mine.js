2a851c84b842e8f7675ef58b1d97217efaf03f12
Andreas Offenhäuser <anoff@users.noreply.github.com>
2017-08-03 00:45:14 +0200

    Update mine.js

--- bin/mine.js
+++ bin/mine.js
@@ -6,13 +6,4 @@ const pkg = require('./package.json')
 program
   .version(pkg.version)
   .option('-d, --data', 'Path to data directory')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
   .parse(process.argv)
-
-console.log('you ordered a pizza with:')
-if (program.peppers) console.log('  - peppers')
-if (program.pineapple) console.log('  - pineapple')
-if (program.bbqSauce) console.log('  - bbq')
-console.log('  - %s cheese', program.cheese)
