5986f53b13a0ae16e8ab2213eca1f9b730026d82
Kevin Huber <kevinahuber@gmail.com>
2017-08-19 11:23:57 -0500

    Rearrange to have a command

--- bin/cli.js
+++ bin/cli.js
@@ -7,13 +7,20 @@ const pckg = require('../package.json')
 program
   .version(pckg.version)
   .usage('[options]')
+  .action(() => {
+    program.help()
+  })
+
+program
+  .command('train')
+  .description('Train your mine locally using a sonar smart contract')
   .option('-m, --mine-address <address>', 'Blockchain address for the mine to use')
   .option('-c, --contract-address <address>', 'Sonar smart contract address for the mine to use')
   .option('-i, --ipfs-url [url]', 'Url of the IPFS node (Default: "{host: \'localhost\', port: \'5001\', protocol: \'http\'})")')
   .option('-e, --ethereum-url [url]', 'Url to the ethereum network to use (Default: "http://localhost:8545")')
   // TODO: Add dev mode with watching
-  .parse(process.argv)
+  .action((options) => {
+    app.checkForModels(options.mineAddress, options.contractAddress, options.ethereumUrl, options.ipfsUrl)
+  })
 
-assert(program.mineAddress, '--mine-address required')
-assert(program.contractAddress, '--contract-address required')
-app.checkForModels(program.mineAddress, program.contractAddress, program.ethereumUrl, program.ipfsUrl)
+program.parse(process.argv)
