b613a2b1742bc4174c35e7e1e179df1242c4240f
aniket965 <aniket965.as@gmail.com>
2017-09-30 18:44:48 +0530

    added ipfs-url check in bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
+
 const url = require('url')
 const program = require('commander')
 const app = require('../mine.js')
 const pckg = require('../package.json')
 const Web3 = require('web3')
 const ipfsAPI = require('ipfs-api')
+const isIPFS = require('is-ipfs')
 
 program
   .version(pckg.version)
@@ -26,7 +28,7 @@ program
   .option('-i, --ipfs-url [url]', 'Url of the IPFS node (Default: "http://localhost:5001")')
   .option('-e, --ethereum-url [url]', 'Url to the ethereum network to use (Default: "http://localhost:8545")')
   // TODO: Add dev mode with watching
-  .action(async (options) => {
+  .action(async(options) => {
     let mineAddress = options.mineAddress
     const contractAddress = options.contractAddress
 
@@ -41,16 +43,20 @@ program
       const mineAddresses = await web3.eth.getAccounts()
       mineAddress = mineAddresses.length && mineAddresses[0]
     }
-
-    const ipfsUrl = options.ipfsUrl || 'http://localhost:5001'
-    // conver to object format as expected by ipfs-api
-    const ipfsUrlOpts = url.parse(ipfsUrl)
-    ipfsUrlOpts.host = ipfsUrlOpts.hostname // use only hostname w/o port
-    const ipfs = ipfsAPI(ipfsUrlOpts)
-
-    app.checkForModels(mineAddress, contractAddress, web3, ipfs)
+    if (isIPFS.ipfsUrl(options.ipfsUrl)) {
+      const ipfsUrl = options.ipfsUrl || 'http://localhost:5001'
+      // conver to object format as expected by ipfs-api
+      const ipfsUrlOpts = url.parse(ipfsUrl)
+      ipfsUrlOpts.host = ipfsUrlOpts.hostname // use only hostname w/o port
+      const ipfs = ipfsAPI(ipfsUrlOpts)
+      app.checkForModels(mineAddress, contractAddress, web3, ipfs)
+    } else {
+      return console.error('Invalid ipfs-url')
+    }
   })
 program
-  .command('help',{isDefault: true})
+  .command('help', {
+    isDefault: true
+  })
 
 program.parse(process.argv)
